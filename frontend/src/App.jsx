f6c264a4d6f24fe87b6e96820d48427f3fc2a21c
brijeshpatolia <bpatolialtc@gmail.com>
2025-03-26 16:50:03 +0530

    notfound page for unkown routes added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import { Route, Routes } from 'react-router-dom';
 import { SignInCard } from '@/components/organisms/Auth/SigninCard';
 import { SignUpCard } from '@/components/organisms/Auth/SignupCard';
 import { Auth } from '@/pages/Auth/Auth';
+import { Notfound } from '@/pages/Notfound/Notfound';
 
 function App() {
   return (
@@ -12,6 +13,8 @@ function App() {
       <Route path="/auth/signup" element={<Auth><SignUpCard /></Auth>} />
 
       <Route path="/auth/signin" element={<Auth><SignInCard /></Auth>} />
+
+      <Route path="/*" element = {<Notfound />} />
     </Routes>
   );
 }
