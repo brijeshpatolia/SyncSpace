160fc0db2962b28fa1a35fb4df6800eb20560b4f
brijeshpatolia <bpatolialtc@gmail.com>
2025-03-26 13:04:25 +0530

    eslint setup done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,14 @@
-import { Button } from "./components/ui/button";
+import './App.css';
 
-export default function App() {
+import { Button } from './components/ui/button';
+
+function App() {
   return (
-    <h1 className="text-3xl font-bold underline text-red-400">
-      Hello world!
-      <Button>hii</Button>
-    </h1>
-  )
+    <div>
+      <Button>hiii</Button>
+    </div>
+  );
 }
+
+
+export default App;
