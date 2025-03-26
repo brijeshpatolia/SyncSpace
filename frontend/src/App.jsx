0d1507fb8ba645ef641cda80681310a74e9b8e46
brijeshpatolia <bpatolialtc@gmail.com>
2025-03-26 12:17:17 +0530

    shadcn added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,10 @@
+import { Button } from "./components/ui/button";
+
 export default function App() {
   return (
     <h1 className="text-3xl font-bold underline text-red-400">
       Hello world!
+      <Button>hii</Button>
     </h1>
   )
 }
